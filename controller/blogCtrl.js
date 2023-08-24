94eda5fd2d61d8a8b209571d9daa177afb37f545
Zardron <zardron@clockworkdelivery.com>
2023-08-24 22:52:00 +0400

    adding blog functionalities

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -2,3 +2,61 @@ const Blog = require("../models/blogModel");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDbId = require("../utils/validateMongoDbId");
+
+// Create New Blog
+const createBlog = asyncHandler(async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json({
+      newBlog,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Get All Blogs
+const getBlog = asyncHandler(async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.json({
+      blogs,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Get Specific Blog
+const getSpecificBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+
+  try {
+    const blogDetails = await Blog.findById(id);
+    await Blog.findByIdAndUpdate(id, { $inc: { numViews: 1 } }, { new: true });
+
+    res.json(blogDetails);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Update Specific Blog
+const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+
+  try {
+    const blogUpdate = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json({
+      blogUpdate,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createBlog, updateBlog, getBlog, getSpecificBlog };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getSpecificBlog,
+} = require("../controller/blogCtrl");
 const router = express.Router();
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/", getBlog);
+router.get("/:id", getSpecificBlog);
 
 module.exports = router;
